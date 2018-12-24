66966a4c75141efe53a8dc794ffb05c03da9c791
Chris Pluta <chris.pluta@yetisoftworks.com>
2018-12-23 18:10:55 -0600

    Created Example for Console Application

---
+++ Examples/MeshyDB.SDK.Examples.Console.Net471/MeshyDB.SDK.Examples.Console.Net471/ExampleData.cs
@@ -0,0 +1,13 @@
+﻿using MeshyDB.SDK.Models;
+
+public class ExampleData : MeshData
+{
+    // Your Mesh Data Properties will go here
+
+    #region Example Properties
+
+    public string Name { get; set; }
+    public int FavoriteNumber { get; set; }
+
+    #endregion
+}
