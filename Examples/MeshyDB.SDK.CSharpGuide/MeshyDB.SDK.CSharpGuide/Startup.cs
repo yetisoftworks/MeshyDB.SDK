def3303a0041a1cd7e72cf8335dda41b3a1aacff
Chris Pluta <chris.pluta@yetisoftworks.com>
2019-01-05 15:31:10 -0600

    Adding solution to follow guide in admin MeshyDB

---
+++ Examples/MeshyDB.SDK.CSharpGuide/MeshyDB.SDK.CSharpGuide/Startup.cs
@@ -0,0 +1,37 @@
+﻿using MeshyDB.SDK.CSharpGuide.Controllers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeshyDB.SDK.CSharpGuide
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            PersonsController.InitializeMeshyDBClient(Configuration["MeshyDB:ClientKey"], Configuration["MeshyDB:PublicKey"], Configuration["MeshyDB:PrivateKey"]);
+        }
+    }
+}
