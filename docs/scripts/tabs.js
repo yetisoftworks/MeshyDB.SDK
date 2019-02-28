dfd0a7ba80dbcc3e165ca75fee20887c6f59b022
cpluta <42399376+cpluta@users.noreply.github.com>
2019-02-28 01:43:23 -0600

    Update tabs.js

--- docs/scripts/tabs.js
+++ docs/scripts/tabs.js
@@ -1,1 +1,9 @@
-$(':input.code-tab').click(function(){$(':input.code-tab[data-lang="'+ $(this).attr('data-lang')+'"]:not(:checked)').not(this).click() })
+$(':input.code-tab').click(function(){
+  const selectedLang = $(this).attr('data-lang');
+  $(':input.code-tab[data-lang="'+ selectedLang  +'"]:not(:checked)').not(this).click();
+  localStorage["selectedLang"] = selectedLang;
+});
+
+if(localStorage["selectedLang"]){
+  $(':input.code-tab[data-lang="'+ localStorage["selectedLang"]  +'"]:not(:checked)').click();
+}
