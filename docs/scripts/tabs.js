0d9304671e069626bd5ca3361e4dd061331e232e
cpluta <42399376+cpluta@users.noreply.github.com>
2019-02-28 01:25:07 -0600

    Update tabs.js

--- docs/scripts/tabs.js
+++ docs/scripts/tabs.js
@@ -1,65 +1,61 @@
     // https://hackernoon.com/fenced-code-tabs-in-mkdocs-material-a7951a9f8738
     document.addEventListener('DOMContentLoaded', function () {
-      var hashcache = {};
-      var langcache = {};
-      var ariacache = {};
-      Array.prototype.forEach.call(document.querySelectorAll('ul.nav.nav-tabs'), function (tablist) {
-        Array.prototype.forEach.call(tablist.querySelectorAll('li.nav-item'), function (tabitem) {
-          Array.prototype.forEach.call(tabitem.querySelectorAll('a'), function (anchor) {
-            var lang = anchor.getAttribute("data-lang");
-            var aria = anchor.getAttribute("aria-controls");
-            var obj = {
-              tabitem: tabitem,
-              anchor: anchor,
-              hash: anchor.hash,
-              lang: lang,
-              aria: aria
-            };
-            hashcache[anchor.hash] = obj;
-            ariacache[aria] = obj;
-            if (!langcache.hasOwnProperty(lang)) {
-              langcache[lang] = [];
-            }
-            langcache[lang].push(obj);
-          });
-        });
-      });
-      Array.prototype.forEach.call(document.querySelectorAll('div.tab-pane'), function (tabpane) {
+	var hashcache = {};
+    var langcache = {};
+    var ariacache = {};
+	Array.prototype.forEach.call(document.querySelectorAll('ul.nav.nav-tabs'), function(tablist) {
+    Array.prototype.forEach.call(tablist.querySelectorAll('li.nav-item'), function(tabitem) {
+      Array.prototype.forEach.call(tabitem.querySelectorAll('a'), function (anchor) {
+        var lang = anchor.getAttribute("data-lang");
+        var aria = anchor.getAttribute("aria-controls");
+        var obj = {
+          tabitem: tabitem,
+          anchor: anchor,
+          hash: anchor.hash,
+          lang: lang,
+          aria: aria
+        };
+        hashcache[anchor.hash] = obj;
+        ariacache[aria] = obj;
+        if (!langcache.hasOwnProperty(lang)) {
+          langcache[lang] = [];
+        }
+        langcache[lang].push(obj);
+		});
+    });
+	});
+    Array.prototype.forEach.call(document.querySelectorAll('div.tab-pane'), function(tabpane) {
         ariacache[tabpane.id].pane = tabpane
-      });
+	});
+	window.addEventListener('hashchange', onhashchange);
 
-      function onhashchange() {
-          console.log(hashcache);
-          console.log(location);
-          console.log(location.hash);
+	onhashchange();
+
+    function onhashchange() {
         var lang = hashcache[location.hash].lang;
-        Array.prototype.forEach.call(Object.keys(langcache), function (maybelang) {
-          if (lang === maybelang) {
-            Array.prototype.forEach.call(langcache[maybelang], function (ooo) {
-              Array.prototype.forEach.call([ooo.tabitem, ooo.pane], function (obj) {
+        Array.prototype.forEach.call(Object.keys(langcache), function(maybelang) {
+        if (lang === maybelang) {
+            Array.prototype.forEach.call(langcache[maybelang], function(ooo) {
+            Array.prototype.forEach.call([ooo.tabitem, ooo.pane], function(obj) {
                 classes = obj.className.split(' ');
                 if (classes.indexOf('active') === -1) {
-                  classes.push('active');
-                  obj.className = classes.join(' ');
+                classes.push('active');
+                obj.className = classes.join(' ');
                 }
-              });
             });
-          } else {
-            Array.prototype.forEach.call(langcache[maybelang], function (ooo) {
-              Array.prototype.forEach.call([ooo.tabitem, ooo.pane], function (obj) {
+            });
+        } else {
+            Array.prototype.forEach.call(langcache[maybelang], function(ooo) {
+            Array.prototype.forEach.call([ooo.tabitem, ooo.pane], function(obj) {
                 classes = obj.className.split(' ');
                 var idx = classes.indexOf('active')
                 if (idx > -1) {
-                  delete classes[idx];
-                  obj.className = classes.join(' ');
+                delete classes[idx];
+                obj.className = classes.join(' ');
                 }
-              });
             });
-          }
+            });
+        }
         });
-      }
-
-      window.addEventListener('hashchange', onhashchange);
-
-      onhashchange();
-    });
+    }
+});
