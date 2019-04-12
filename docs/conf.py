55f80d6ffd3279bbe658dc2ab680654acf0be995
cpluta <42399376+cpluta@users.noreply.github.com>
2019-04-12 13:19:06 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', 'scripts/requirehttps.js']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -99,7 +99,7 @@ html_static_path = ['_static']
 #
 # html_sidebars = {}
 
-html_js_files = ['scripts/requirehttps.js']
+#  html_js_files = ['scripts/requirehttps.js']
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
