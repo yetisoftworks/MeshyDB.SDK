5cbc19aad36fcc16fd956b69f083b1e52264f2fb
Chris Pluta <chris.pluta@yetisoftworks.com>
2019-07-08 19:30:01 -0500

    Changing back to proper names since css looks fine

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', 'scripts/requirehttps.js', 'styles/parameters2.css']
+html_static_path = ['_static', 'scripts/requirehttps.js', 'styles/parameters.css']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -99,7 +99,7 @@ html_static_path = ['_static', 'scripts/requirehttps.js', 'styles/parameters2.cs
 #
 # html_sidebars = {}
 
-html_css_files = ['parameters2.css']
+html_css_files = ['parameters.css']
 
 html_js_files = ['requirehttps.js']
 
